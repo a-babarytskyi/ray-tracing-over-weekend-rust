75a76857870e9fe2b15ff2b2847b505015b7c9f0
Andrii Babarytskyi <andrii.babarytskyi@epfl.ch>
2025-09-24 10:36:17 +0200

    Hittable trait, ground sphere

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,30 @@
+use core::f64;
+use std::sync::Arc;
 use std::thread;
-use std::time::Duration;
 
 mod color;
 mod ray;
 mod vec3;
+mod hittables;
 
 use ray::Ray;
 use std::time::Instant;
 use vec3::Vec3;
 
+use hittables::{ HitRecord, Sphere, HittableList};
+
 use Vec3 as Color;
 use Vec3 as Point3;
 
-pub fn hit_sphere(center: &Point3, radius: f32, ray: &Ray) -> f32 {
-    let co = *center - ray.origin();
-    let a = &ray.direction().length_squared();
-    let h = Vec3::dot(&ray.direction(), &co);
-    let c = co.length_squared() - radius * radius;
 
-    let discriminant = h * h - a * c;
 
-    if discriminant >= 0.0 {
-        return (h - discriminant.sqrt()) / (a);
-    } else {
-        return -1.0;
+pub fn ray_color(r: &Ray, world: &HittableList) -> Color {
+
+    let mut hit_rec = HitRecord::new();
+    if world.hit(r, 0.0, f64::INFINITY, &mut hit_rec) {
+        return 0.5 * (hit_rec.normal + Color::from_values(1.0, 1.0, 1.0));
     }
-}
-
-pub fn ray_color(r: Ray) -> Color {
-    let sphere_center = Point3::from_values(0.0, 0.0, -1.0);
-
-    let t = hit_sphere(&sphere_center, 0.5, &r);
-
-    if t >= 0.0 {
-        let x = r.at(t) - sphere_center;
-        let n = Vec3::unit_vector(&x);
-        return 0.5 * Color::from_values(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
-    }
-
+    
     let unit_direction = Vec3::unit_vector(&r.direction());
     let a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * Color::from_values(1.0, 1.0, 1.0) + a * Color::from_values(0.5, 0.7, 1.0);
@@ -53,17 +40,18 @@ pub fn calculate_chank(
     camera_center: Vec3,
     pixel_delta_u: Vec3,
     pixel_delta_v: Vec3,
+    world: &Arc<HittableList>,
 ) -> String {
     let mut chunk_res = String::new();
     for j in y_start..y_end {
         for i in x_start..x_end {
             let pixel_center =
-                pixel00_loc + (i as f32 * pixel_delta_u) + (j as f32 * pixel_delta_v);
+                pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
 
             let ray_direction = pixel_center - camera_center;
             let r = Ray::new(camera_center, ray_direction);
 
-            let pixel_color = ray_color(r);
+            let pixel_color = ray_color(&r, &world);
 
             chunk_res.push_str(color::write_color(pixel_color).as_str());
         }
@@ -78,14 +66,14 @@ fn main() {
 
     // Calculate the image height, and ensure that it's at least 1.
 
-    let mut image_height: i32 = (image_width as f32 / aspect_ratio) as i32;
+    let mut image_height: i32 = (image_width as f64 / aspect_ratio) as i32;
     image_height = if image_height < 1 { 1 } else { image_height };
 
     // Camera
 
     let focal_length = 1.0;
     let viewport_height = 2.0;
-    let viewport_width = viewport_height * (image_width as f32 / image_height as f32);
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
     let camera_center = Point3::zero();
 
     // Calculate the vectors across the horizontal and down the vertical viewport edges.
@@ -93,10 +81,24 @@ fn main() {
     let viewport_u = Vec3::from_values(viewport_width, 0.0, 0.0);
     let viewport_v = Vec3::from_values(0.0, -viewport_height, 0.0);
 
+    // World
+
+    let mut world = HittableList::new();
+    world.add(Box::new(Sphere::new(
+        Point3::from_values(0.0, 0.0, -1.0),
+        0.5,
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::from_values(0.0, -100.5, -1.0),
+        100.0,
+    )));
+
+    let world = Arc::new(world);
+
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
 
-    let pixel_delta_u = viewport_u / image_width as f32;
-    let pixel_delta_v = viewport_v / image_height as f32;
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
 
     // Calculate the location of the upper left pixel.
 
@@ -143,6 +145,8 @@ fn main() {
                 index, start_x, end_x, start_y, end_y
             );
 
+            let world_ref = Arc::clone(&world);
+
             handles.push(thread::spawn(move || {
                 (
                     index,
@@ -156,6 +160,7 @@ fn main() {
                         camera_center,
                         pixel_delta_u,
                         pixel_delta_v,
+                        &world_ref,
                     ),
                 )
             }));
--- src/vec3.rs
+++ src/vec3.rs
@@ -2,7 +2,7 @@ use core::fmt;
 use std::ops;
 
 pub struct Vec3 {
-    e: [f32; 3],
+    e: [f64; 3],
 }
 
 impl fmt::Display for Vec3 {
@@ -16,27 +16,27 @@ impl Vec3 {
         Vec3 { e: [0.0; 3] }
     }
 
-    pub fn from_values(e0: f32, e1: f32, e2: f32) -> Vec3 {
+    pub fn from_values(e0: f64, e1: f64, e2: f64) -> Vec3 {
         Vec3 { e: [e0, e1, e2] }
     }
 
-    pub fn x(&self) -> f32 {
+    pub fn x(&self) -> f64 {
         self.e[0]
     }
 
-    pub fn y(&self) -> f32 {
+    pub fn y(&self) -> f64 {
         self.e[1]
     }
 
-    pub fn z(&self) -> f32 {
+    pub fn z(&self) -> f64 {
         self.e[2]
     }
 
-    pub fn length_squared(&self) -> f32 {
+    pub fn length_squared(&self) -> f64 {
         self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
     }
 
-    pub fn length(&self) -> f32 {
+    pub fn length(&self) -> f64 {
         self.length_squared().sqrt()
     }
 
@@ -44,13 +44,13 @@ impl Vec3 {
         *vec / vec.length()
     }
 
-    pub fn dot(vec1: &Self, vec2: &Self) -> f32 {
+    pub fn dot(vec1: &Self, vec2: &Self) -> f64 {
         return vec1.e[0] * vec2.e[0] + vec1.e[1] * vec2.e[1] + vec1.e[2] * vec2.e[2];
     }
 }
 
-impl ops::MulAssign<f32> for Vec3 {
-    fn mul_assign(&mut self, rhs: f32) {
+impl ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
         self.e[0] += rhs;
         self.e[1] += rhs;
         self.e[2] += rhs;
@@ -65,8 +65,8 @@ impl ops::AddAssign for Vec3 {
     }
 }
 
-impl ops::DivAssign<f32> for Vec3 {
-    fn div_assign(&mut self, rhs: f32) {
+impl ops::DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
         *self *= 1.0 / rhs;
     }
 }
@@ -82,7 +82,7 @@ impl ops::Neg for Vec3 {
 }
 
 impl ops::Index<usize> for Vec3 {
-    type Output = f32;
+    type Output = f64;
 
     fn index(&self, index: usize) -> &Self::Output {
         &self.e[index]
@@ -123,17 +123,17 @@ impl ops::Sub for Vec3 {
     }
 }
 
-impl ops::Mul<f32> for Vec3 {
+impl ops::Mul<f64> for Vec3 {
     type Output = Vec3;
 
-    fn mul(self, rhs: f32) -> Self::Output {
+    fn mul(self, rhs: f64) -> Self::Output {
         Vec3 {
             e: [self.e[0] * rhs, self.e[1] * rhs, self.e[2] * rhs],
         }
     }
 }
 
-impl ops::Mul<Vec3> for f32 {
+impl ops::Mul<Vec3> for f64 {
     type Output = Vec3;
 
     fn mul(self, rhs: Vec3) -> Self::Output {
@@ -141,10 +141,10 @@ impl ops::Mul<Vec3> for f32 {
     }
 }
 
-impl ops::Div<f32> for Vec3 {
+impl ops::Div<f64> for Vec3 {
     type Output = Vec3;
 
-    fn div(self, rhs: f32) -> Self::Output {
+    fn div(self, rhs: f64) -> Self::Output {
         self * (1.0 / rhs)
     }
 }
